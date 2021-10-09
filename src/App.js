c25400cdae3112205318408238bfb67f159f18c6
Matuska <pmatuska@amazon.com>
2021-10-09 15:29:21 -0400

    Added Text

--- src/App.js
+++ src/App.js
@@ -1,14 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+
 import './App.css';
+import Amplify, { Storage, Predictions } from 'aws-amplify';
+import { AmazonAIPredictionsProvider } from '@aws-amplify/predictions';
+
+import awsconfig from './aws-exports';
+
+Amplify.configure(awsconfig);
+Amplify.addPluggable(new AmazonAIPredictionsProvider());
+
+
+function TextTranslation() {
+  const [response, setResponse] = useState("Input some text and click enter to test")
+  const [textToTranslate, setTextToTranslate] = useState("write to translate");
+
+  function translate() {
+    Predictions.convert({
+      translateText: {
+        source: {
+          text: textToTranslate,
+          // language : "es" // defaults configured on aws-exports.js
+          // supported languages https://docs.aws.amazon.com/translate/latest/dg/how-it-works.html#how-it-works-language-codes
+        },
+        // targetLanguage: "en"
+      }
+    }).then(result => setResponse(JSON.stringify(result, null, 2)))
+      .catch(err => setResponse(JSON.stringify(err, null, 2)))
+  }
+
+  function setText(event) {
+    setTextToTranslate(event.target.value);
+  }
+
+  return (
+    <div className="Text">
+      <div>
+        <h3>Text Translation</h3>
+        <input value={textToTranslate} onChange={setText}></input>
+        <button onClick={translate}>Translate</button>
+        <p>{response}</p>
+      </div>
+    </div>
+  );
+}
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello from V2</h1>
-      </header>
+      Translate Text
+      <TextTranslation />
     </div>
   );
 }
